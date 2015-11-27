c7d11967d108f0ffe1f2595cc7c934c0fcd1a358
Johnny Chang <johnny@johnnychang.com>
2015-11-26 23:20:11 -0800

    Moved around tracks column and fixed formatting.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var bassify = angular.module("bassify", []);
-
 var bootstrapModule = angular.module("bootstrapModule", []);
 
 function get(key, object) {
@@ -11,12 +10,11 @@ bootstrapModule.factory("bootstrapper", function($q, $timeout){
     return {
         bootstrap: function (appName){
             var deferred = $q.defer();
-
             var myApp = angular.module(appName);
             var mopidy = new Mopidy({"callingConvention": "by-position-only"});
+
             $timeout(mopidy.on("state:online", function() {
                 myApp.constant('mopidy', mopidy);
-                console.log("Mopidy a constant");
                 angular.bootstrap(document, [appName]);
                 deferred.resolve();
             }), 2000);
@@ -28,6 +26,7 @@ bootstrapModule.factory("bootstrapper", function($q, $timeout){
 
 // Used as root for bootstrap app.
 var appContainer = document.createElement('div');
+
 bootstrapModule.run(function (bootstrapper) {
     bootstrapper.bootstrap("bassify").then(function() {
         // Destroy bootstrap app when done.
@@ -35,62 +34,65 @@ bootstrapModule.run(function (bootstrapper) {
     });
 });
 
+// On document ready, start the bootstrapModule.
 angular.element(document).ready(function() {
     angular.bootstrap(appContainer, ["bootstrapModule"]);
 });
 
 bassify.controller('TracklistController', function($scope, mopidy) {
-        var self = this;
-        mopidy.on("event:trackPlaybackStarted", function() {
-            mopidy.tracklist.getTlTracks()
-                .then(getTracklistNames);
-        });
-        
-        function extractTrackFromTlTrack(tlTrack, index, tracks) {
-            return {
-                trackName: tlTrack.track.name,
-                artists: tlTrack.track.artists,
-                album: tlTrack.track.album.name,
-                length: tlTrack.track.length,
-                uri: tlTrack.track.uri
-            };
-        }
+    var self = this;
 
-        function getTracklistNames(tracks) {
-            console.log(tracks);
-            $scope.$apply(function() {
-                self.tracks = tracks.map(extractTrackFromTlTrack);
-            });
-        }
-    })
-    .controller('PlaylistController', function($scope, mopidy){
-        var self = this;
-
-        function getPlaylistNames(playlists) {
-            $scope.$apply(function() {
-                console.log(playlists);
-                self.playlists = playlists;
-            });
-        }
-
-        function extractPlaylistItems(items) {
-            self.currentTracks = items;
-        }
-
-        self.getPlaylistTracks = function (uri) {
-            mopidy.playlists.getItems(uri)
-                  .done(extractPlaylistItems);
-        };
-
-        self.addTrack = function (uri) {
-            //mopidy.library.lookup(uri)
-            //      .done(playTrack);
-            mopidy.tracklist
-                  .add(null, null, uri, null)
-                  .fold(get, 0)
-                  .done(mopidy.playback.play);
-        }
-
-        mopidy.playlists.getPlaylists()
-            .done(getPlaylistNames);
+    mopidy.on("event:trackPlaybackStarted", function() {
+        mopidy.tracklist.getTlTracks()
+            .then(getTracklistNames);
     });
+
+    function extractTrackFromTlTrack(tlTrack, index, tracks) {
+        return {
+            trackName: tlTrack.track.name,
+            artists: tlTrack.track.artists,
+            album: tlTrack.track.album.name,
+            length: tlTrack.track.length,
+            uri: tlTrack.track.uri
+        };
+    }
+
+    function getTracklistNames(tracks) {
+        console.log(tracks);
+        $scope.$apply(function() {
+            self.tracks = tracks.map(extractTrackFromTlTrack);
+        });
+    }
+});
+
+bassify.controller('PlaylistController', function($scope, mopidy){
+    var self = this;
+
+    function getPlaylistNames(playlists) {
+        $scope.$apply(function() {
+            console.log(playlists);
+            self.playlists = playlists;
+        });
+    }
+
+    function extractPlaylistItems(items) {
+        self.currentTracks = items;
+    }
+
+    self.getPlaylistTracks = function (uri) {
+        mopidy.playlists.getItems(uri)
+              .done(extractPlaylistItems);
+    };
+
+    self.addTrack = function (uri) {
+        //mopidy.library.lookup(uri)
+        //      .done(playTrack);
+        mopidy.tracklist
+              .add(null, null, uri, null)
+              .fold(get, 0)
+              .done(mopidy.playback.play);
+    }
+
+    mopidy.playlists.getPlaylists()
+        .done(getPlaylistNames);
+});
